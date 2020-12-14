3968e6cc66919278f9226651a031043ab9f211fc
Ahmad Supriyanto <ahmad.supriyanto@ui.ac.id>
2020-12-14 20:33:04 +0700

    Change configuration without expo package

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.greetingContainer}>
-        <Text style={{color: "#fff", fontWeight: "bold"}}>Hello world!</Text>
-        <Text style={{color: "#fff", fontWeight: "bold"}}>My Name is Ahmad Supriyanto!</Text>
-      </View>
-      <StatusBar style="auto" />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <View style={styles.greetingContainer}>
+          <Text style={{color: "#fff", fontWeight: "bold"}}>Hello world!</Text>
+          <Text style={{color: "#fff", fontWeight: "bold"}}>My Name is Ahmad Supriyanto!</Text>
+        </View>
     </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -22,10 +36,11 @@ const styles = StyleSheet.create({
   },
   greetingContainer: {
     backgroundColor: '#008577',
-    padding: "10px",
     width: "100%",
     height: "30%",
     alignItems: "center",
     justifyContent: "center"
   }
 });
+
+export default App;
